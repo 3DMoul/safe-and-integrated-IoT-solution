e9be2e1217fdd62851fdaf095f6790e9033818df
Svante Norrhäll <svanteman123@yahoo.se>
2026-09-11 15:04:05 +0200

    setting up main for sensor reading

---
+++ src/main.c
@@ -0,0 +1,33 @@
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+#include <time.h>
+#include "lcd.h"
+#include "millis.h"
+#include "uart.h"
+
+#define BIT_SET(a, b) ((a) |= (1ULL << (b)))
+#define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
+#define BIT_FLIP(a,b) ((a) ^= (1ULL<<(b)))
+#define BIT_CHECK(a,b) (!!((a) & (1ULL<<(b)))) 
+
+// B (digital pin 8 to 13)
+// C (analog input pins)
+// D (digital pins 0 to 7)
+// https://wokwi.com/projects/363784064760337409
+
+
+
+int main(void)
+{
+    millis_init();
+	init_serial();
+	timer1_init();
+	srand(time(NULL));
+	lcd_init();
+    sei();
+	while(1) {
+    }
+	return 0;
+}
