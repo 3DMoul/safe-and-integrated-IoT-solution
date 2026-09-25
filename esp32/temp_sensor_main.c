27e8906a5bb8a6cfbf95f393e1707083f1f34a2c
Svante Norrhäll <svanteman123@yahoo.se>
2026-09-25 14:32:33 +0200

    fixed typos and added wifi init

--- esp32/temp_sensor_main.c
+++ esp32/temp_sensor_main.c
@@ -4,11 +4,11 @@
 #include "freertos/task.h"
 
 #include "esp_log.h"
-#include "esp.err.h"
-#include "nvs.flash.h"
-#include "esp.netif.h"
-#include "esp.event.h"
-#include "esp.wifi.h"
+#include "esp_err.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "esp_event.h"
+#include "esp_wifi.h"
 
 #include "esp_adc/adc_oneshot.h"
 #include "mqtt_client.h"
@@ -20,14 +20,83 @@ static const char *TAG = "MCP9700A";
 
 // wifi setup
 #include "wifisecrets.h" //wifi information
-#define WIFI_SSID "Tele2_9A1706_EXT2.4Gz"
-#define WIFI_PASSWORD "db"
+
+#define WIFI_CONNECTED_BIT BIT0
+
+static void wifi_event_handler( void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+
+        ESP_LOGI(TAG, "Wi-Fi started, connecting...");
+        ESP_ERROR_CHECK(esp_wifi_connect());
+
+    }
+    else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+
+        ESP_LOGW(TAG, "Wi-Fi disconnected, retrying...");
+        esp_wifi_connect();
+
+    }
+    else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI( TAG, "Got IP address: " IPSTR, IP2STR(&event->ip_info.ip));
+        xEventGroupSetBits( wifi_event_group, WIFI_CONNECTED_BIT );
+    }
+}
+
+static void wifi_init(void) {
+
+    wifi_event_group = xEventGroupCreate();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK( esp_event_loop_create_default() );
+
+    esp_netif_create_default_wifi_sta();
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_event_handler_register( WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL ) );
+    ESP_ERROR_CHECK( esp_event_handler_register( IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL ) );
+
+    wifi_config_t wifi_config = {
+                .sta = 
+            {   .ssid = WIFI_SSID,
+                .password = WIFI_PASSWORD,
+                .threshold.authmode = WIFI_AUTH_WPA2_PSK, }, 
+            };
+    
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config( WIFI_IF_STA, &wifi_config ) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+
+    ESP_LOGI(TAG, "Waiting for Wi-Fi connection...");
+
+    xEventGroupWaitBits( 
+        wifi_event_group,
+        WIFI_CONNECTED_BIT,
+        pdFALSE,
+        pdTRUE,
+        portMAX_DELAY );
+
+    ESP_LOGI(TAG, "Wi-Fi connection established.");
+
+}
+
 
 void app_main(void)
 {
-    // -------------------------
+
+    // NVS initialization
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ESP_ERROR_CHECK(nvs_flash_init());
+    }
+
+    // Wi-Fi setup
+    wifi_init();
+
     // MQTT setup
-    // -------------------------
     esp_mqtt_client_config_t mqtt_cfg = {
         .broker.address.uri = "mqtt://192.168.1.123:1883",
     };
@@ -37,9 +106,7 @@ void app_main(void)
 
     ESP_ERROR_CHECK(esp_mqtt_client_start(client));
 
-    // -------------------------
     // ADC setup
-    // -------------------------
     adc_oneshot_unit_handle_t adc_handle;
 
     adc_oneshot_unit_init_cfg_t init_config = {
@@ -63,9 +130,7 @@ void app_main(void)
         )
     );
 
-    // -------------------------
     // Main sensor loop
-    // -------------------------
     while (1) {
 
         int raw;
